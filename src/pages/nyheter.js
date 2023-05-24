9cf9fd2f5332b8376436cddab5672f2ad7a9bc3d
Ludde2222 <129958006+Ludde2222@users.noreply.github.com>
2023-05-24 11:59:18 +0200

    fullt responsive

---
+++ src/pages/nyheter.js
@@ -0,0 +1,20 @@
+import "../App.css";
+import { useState } from "react";
+
+function Test() {
+  return (
+    <>
+      <h1>test</h1>
+    </>
+  );
+}
+
+export function Nyheter() {
+  return (
+    <>
+      <Test />
+    </>
+  );
+}
+
+export default Contact;
